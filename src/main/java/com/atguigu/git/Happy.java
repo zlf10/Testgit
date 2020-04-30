969aecd69cf5ee9c8e08b0163bceb9187aab51f4
tom_glb <goodMorning_glb@atguigu.com>
2020-04-30 09:02:34 +0800

    test right conflict

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -1,7 +1,7 @@
 package com.atguigu.git;
 
 public class Happy {
-  public static void main(String[] args) {
-	System.out.println("left ....");
- }
+	public static void main(String[] args) {
+		System.out.println("right ...1111");
+	 }
 }
